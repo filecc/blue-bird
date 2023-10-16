cb754f2747f02f6e0ae1b7a93494aff3ac73ed01
filecc <fil.gia@live.it>
2023-10-16 14:18:33 +0200

    signin and signout

---
+++ app/components/auth-button.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
+
+export default function AuthButton() {
+    const supabase = createClientComponentClient()
+
+    const handleSignin = async () => {
+        await supabase.auth.signInWithOAuth({
+            provider: 'github',
+            options: {
+                redirectTo: 'http://localhost:3000/auth/callback'
+            }
+        })
+       
+      }
+
+    const handleSignout = async () => {
+        await supabase.auth.signOut()
+       
+    }
+
+      return (
+        <>
+       
+            <button onClick={handleSignin}>Login</button>
+            
+            <button onClick={handleSignout}>Logout</button>
+        
+        </>
+        
+      )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import Image from 'next/image'
 import { cookies } from 'next/headers'
+import AuthButton from './components/auth-button'
 
 export default async function Home() {
   const supabase = createServerComponentClient({cookies})
   const { data: tweets} = await supabase.from('tweets').select()
 
+  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <AuthButton />
       <pre>{JSON.stringify(tweets, null, 2)}</pre>
     </main>
   )
