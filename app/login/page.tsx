e4d9d38adb970ec01e061bf992ec2a09bb5bf895
filecc <fil.gia@live.it>
2023-10-17 08:30:00 +0200

    protected route and tweet only from authenticated users

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,11 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import AuthButton from "../components/auth-button";
-import { Database } from "@/lib/database.types";
+import AuthButton from "@/app/components/auth-button";
+
 
 export default async function Login() {
-    const supabase = createServerComponentClient<Database>({ cookies })
+    const supabase = createServerComponentClient({ cookies })
 
     const { data: { session }} = await supabase.auth.getSession()
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,27 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import Image from 'next/image'
-import { cookies } from 'next/headers'
-import AuthButtonServer from './components/auth-button-server'
-import { redirect } from 'next/navigation'
-
-
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import AuthButtonServer from "@/app/components/auth-button-server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const supabase = createServerComponentClient<Database>({cookies})
+  const supabase = createServerComponentClient<Database>({ cookies });
 
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-  const { data: { session }} = await supabase.auth.getSession()
-
-  if(!session) {
-    redirect('/login')
+  if (!session) {
+    redirect("/login");
   }
 
-  const { data: tweets} = await supabase.from('tweets').select()
-
-  
+  const { data: tweets } = await supabase
+    .from("tweets")
+    .select("*, profiles(*)")
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <AuthButtonServer />
-     <pre>{JSON.stringify(tweets, null, 2)}</pre>
-    </main>
-  )
+    <>
+      <AuthButtonServer />
+      <pre>{JSON.stringify(tweets, null, 2)}</pre>
+    </>
+  );
 }
