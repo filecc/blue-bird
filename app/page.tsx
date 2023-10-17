b275020e73530cb408e0f001f7ae622965bf2450
filecc <fil.gia@live.it>
2023-10-17 08:40:22 +0200

    inserting a new tweet

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import AuthButtonServer from "@/app/components/auth-button-server";
 import { redirect } from "next/navigation";
+import NewTweet from "./components/new-tweet";
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -20,6 +21,7 @@ export default async function Home() {
 
   return (
     <>
+      <NewTweet />
       <AuthButtonServer />
       <pre>{JSON.stringify(tweets, null, 2)}</pre>
     </>
